b25adad5ff5f6bd36e519490e94186c838659a87
BeansAndPeas <72360102+BeansAndPeas@users.noreply.github.com>
2021-11-17 16:30:25 +0000

    Movement

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -1,19 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
     private new Rigidbody rigidbody;
+    private bool isOnGround;
+
+    void OnCollisionEnter(Collision other) {
+        if(other.gameObject.CompareTag("Ground")) this.isOnGround = true;
+    }
+
+    void OnCollisionExit(Collision other) {
+        if(other.gameObject.CompareTag("Ground")) this.isOnGround = false;
+    }
 
     void Start() {
         this.rigidbody = GetComponent<Rigidbody>();
     }
 
-    void Update() {
+    void FixedUpdate() {
         if(PressingForward()) {
-            this.rigidbody.AddForce(transform.forward);
+            this.rigidbody.AddForce(Vector3.forward * 10);
+        } else if(PressingBackwards()) {
+            this.rigidbody.AddForce(Vector3.back * 10);
+        }
+
+        if(PressingLeft()) {
+            this.rigidbody.AddForce(Vector3.left * 10);
+        } else if(PressingRight()) {
+            this.rigidbody.AddForce(Vector3.right * 10);
+        }
+
+        if(PressingSpace()) {
+            this.rigidbody.AddForce(Vector3.up * 100);
         }
     }
 
-    bool PressingForward() => Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow);
+    bool PressingForward() => Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+    bool PressingBackwards() => Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow);
+    bool PressingLeft() => Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow);
+    bool PressingRight() => Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow);
+    bool PressingSpace() => Input.GetKey(KeyCode.Space);
 }
